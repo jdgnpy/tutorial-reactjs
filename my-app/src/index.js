59cfa51f42c2ba99f7c927fee27685e8e84e84c4
jdgnpy <616dona@gmail.com>
2021-11-10 15:16:58 -0300

    Add - funcion para verificar si hay ganador

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -57,8 +57,16 @@ class Board extends React.Component {
     }
   
     render() {
-      const status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
-  
+      
+      //se verifica si hay algun ganador, en caso de que no, se muestra el turno del siguiente
+      const winner = calculateWinner(this.state.squares);
+      let status;
+      if(winner){
+        status = 'Winner ' + winner;
+      }else{
+        status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
+      }
+
       return (
         <div>
           <div className="status">{status}</div>
@@ -105,3 +113,31 @@ ReactDOM.render(
     <Game />,
     document.getElementById('root')
 );
+
+
+function calculateWinner(squares){
+  const lines = [ //array con las formas posibles para ganar
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for(let i= 0; i< lines.length; i++){ //recorre el array de lines
+    const [a,b,c] = lines[i]; //cargo los valores de cada fila en cada variable
+    
+    // verifica si los valores son iguales en las posiciones de las filas del array lines
+    // ej: si X esta en las posiciones a=0, b=3 y c=6 (diagonal, linea 4 del array lines)
+    // entonces retorna X
+    // asi hasta encontrar alguno que encaje el mismo valor en las 3 posiciones
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){ 
+      return squares [a];
+    }
+  }
+
+  return null;
+}
